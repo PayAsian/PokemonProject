d069dbe3b4ce43cc260baa30ac680196223a8970
PayAsian <PayAsian#yahoo.com>
2016-12-07 13:45:03 -0700

    started to add code for the poked

--- src/poke/model/Pokemon.java
+++ src/poke/model/Pokemon.java
@@ -14,44 +14,44 @@ public abstract class Pokemon
 		
 	}
 	
-	public String getPokemonTypes()
-	{
-		
-	}
-	
 	public String toString()
 	{
-		
+		return toString();
 	}
 	
+	public String getPokemonTypes()
+	{
+		return getPokemonTypes();
+	}
+		
 	public String getPokemonInformation()
 	{
-		
+		return getPokemonInformation();
 	}
 	
 	public int getHealthPoints()
 	{
-		
+		return getHealthPoints();
 	}
 	
 	public int getAttackPoints()
 	{
-		
+		return getAttackPoints();
 	}
 	
 	public double getSpeed()
 	{
-		
+		return getSpeed();
 	}
 	
 	public int getNumber()
 	{
-		
+		return getNumber();
 	}
 	
 	public String getName()
 	{
-		
+		return getName();
 	}
 	
 	public void setHealthPoints(int healthPoints)
